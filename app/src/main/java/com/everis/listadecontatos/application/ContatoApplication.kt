3c8f42f81f722b38ef5f9f9934a0b0937cb7871e
Reinaldo <reinaldonetof@hotmail.com>
2021-04-08 21:47:28 -0300

    Create table, application

--- app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
+++ app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
@@ -3,18 +3,13 @@ package com.everis.listadecontatos.application
 import android.app.Application
 import com.everis.listadecontatos.helpers.HelperDB
 
-class ContatoApplication : Application() {
+class ContatoApplication: Application() {
 
     var helperDB: HelperDB? = null
-        private set
-
-    companion object {
-        lateinit var instance: ContatoApplication
-    }
+        private  set
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
         helperDB = HelperDB(this)
     }
 }
